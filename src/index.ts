752471919cb68d9cc04df650bdb0f6db33029fca
Confrontend <hamed.fatehi.66@gmail.com>
2023-01-21 15:40:22 +0100

    getting  ui-library to work with webpack

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import Navigation from "./components/navigation/navigation";
-import NonIdealState from "./components/non-ideal-state/non-ideal-state";
-import SignIn from "./components/signin/signin";
-import SignOut from "./components/signout/signout";
-import UiButton from "./components/button/ui-button";
-
-export { Navigation, NonIdealState, SignIn, SignOut, UiButton };
