1f51f7f51549fd4b9ed6e8953b00ddbed9829310
Confrontend <hamed.fatehi.66@gmail.com>
2023-04-18 13:13:49 +0200

    Improve ScrollProgressBar

--- src/components/scroll-progress-bar/scroll-progress-bar.tsx
+++ src/components/scroll-progress-bar/scroll-progress-bar.tsx
@@ -5,31 +5,23 @@ type ScrollProgressBarProps = {
   color?: string;
 };
 
-const getScrollPercentage = (): number => {
-  const windowHeight = window.innerHeight;
-  const bodyHeight = document.body.clientHeight;
-  const scrollTop = window.scrollY;
-  const scrollPercentage = (scrollTop / (bodyHeight - windowHeight)) * 100;
-  return scrollPercentage;
-};
-
-const ScrollProgressBar: React.FunctionComponent<ScrollProgressBarProps> = ({
-  color = "blue",
-}) => {
+function ScrollProgressBar({ color = "red" }: ScrollProgressBarProps) {
   const progressBarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const updateProgressBar = () => {
-      if (progressBarRef.current) {
-        const scrollPercentage = getScrollPercentage();
-        progressBarRef.current.style.width = `${scrollPercentage}%`;
-      }
+    if (!progressBarRef.current) {
+      return;
+    }
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop;
+      const windowHeight = window.innerHeight;
+      const docHeight = document.documentElement.scrollHeight - windowHeight;
+      const scrollPercentage = (scrollTop / docHeight) * 100;
+      progressBarRef.current!.style.width = `${scrollPercentage}%`;
     };
 
-    window.addEventListener("scroll", updateProgressBar);
-    return () => {
-      window.removeEventListener("scroll", updateProgressBar);
-    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
@@ -41,6 +33,6 @@ const ScrollProgressBar: React.FunctionComponent<ScrollProgressBarProps> = ({
       />
     </S.ProgressBarWrapper>
   );
-};
+}
 
 export default ScrollProgressBar;
