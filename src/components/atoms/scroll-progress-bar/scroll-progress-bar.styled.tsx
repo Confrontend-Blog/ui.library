bb241296fe44cbb8aa3dcd14ad5546865f5d94e7
Confrontend <hamed.fatehi.66@gmail.com>
2023-07-09 13:22:11 +0200

    Add tooltip stories

--- src/components/atoms/scroll-progress-bar/scroll-progress-bar.styled.tsx
+++ src/components/atoms/scroll-progress-bar/scroll-progress-bar.styled.tsx
@@ -1,19 +1,19 @@
-import { styled } from "@mui/system";
+import { styled } from "../../../utils/styled";
 
-export const ProgressBarWrapper = styled('div')({
-  position: 'fixed',
+export const ProgressBarWrapper = styled("div")({
+  position: "fixed",
   top: 0,
   left: 0,
-  width: '100%',
-  height: '4px',
+  width: "100%",
+  height: "4px",
   zIndex: 100,
 });
 
-export const ProgressBar = styled('div')(({ color }) => ({
-  position: 'absolute',
+export const ProgressBar = styled("div")(({ color }) => ({
+  position: "absolute",
   top: 0,
   left: 0,
-  height: '100%',
+  height: "100%",
   backgroundColor: color,
-  transition: 'width 0.25s ease-in-out',
+  transition: "width 0.25s ease-in-out",
 }));
---
+++ src/components/atoms/tooltip/tooltip.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Tooltip } from "./tooltip";
+
+const meta: Meta<typeof Tooltip> = {
+  title: "Tooltip",
+  component: Tooltip,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = (args) => (
+  <Tooltip {...args}>
+    <span>Hover over me</span>
+  </Tooltip>
+);
+Default.args = {
+  title: "This is a Tooltip!",
+};
